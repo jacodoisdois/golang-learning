e4cc6ded7e13f36e2d7880d78854b930b36df383
Jacó Magalhães <jacomagalhaes@gmail.com>
2023-06-04 18:55:26 -0300

    Lesson 16 - Variables inside if's

---
+++ control-function/03/03.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func randomNumber() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	return r.Intn(10)
+}
+
+func main() {
+
+	if i := randomNumber(); i > 5 {
+		fmt.Println("Won!")
+	} else {
+		fmt.Println("Lose!")
+	}
+
+}
